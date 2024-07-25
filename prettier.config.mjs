556e4f9467e55ba80b959fb494457a63e0e2e114
hjpark625 <zero950@gmail.com>
2024-07-25 16:28:43 +0900

    refactor: prettier mjs 형식으로 변경

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  singleQuote: true,
+  semi: false,
+  tabWidth: 2,
+  printWidth: 120,
+  trailingComma: 'none',
+  useTabs: false
+}
+
+export default config
